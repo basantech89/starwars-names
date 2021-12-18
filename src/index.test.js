ab9a1e9b27f5b4d0c75f51a88987cf60eff8931a
Basant Soni <basant.soni@velotio.com>
2021-12-18 14:34:21 +0530

    build(releasing): add travis config, conventional commit and semantic-release
    This is a longer description
    
    BREAKING CHANGE: Add release scripts
    
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect
+var starWars = require('./index')
+
+describe('starwars-name', function () {
+    describe('all', function () {
+        it('should be an array of strings', function () {
+            expect(starWars.all).to.satisfy(isArrayOfStrings)
+
+            function isArrayOfStrings(array) {
+                return array.every(item => typeof item === "string")
+            }
+        });
+    })
+})
