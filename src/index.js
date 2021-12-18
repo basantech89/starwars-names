12b4d4d84010b164c2ddcb1fdcec2df64e368c24
Basant Soni <basant.soni@velotio.com>
2021-12-18 14:49:09 +0530

    feat(random): add ability to get an array of random names
    If you pass a number to the radom function blabalbal
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 var uniqueRandomArray = require('unique-random-array')
 var starWarsNames = require('./starwars-names.json')
+var getRandomItem = uniqueRandomArray(starWarsNames)
+
+function random(number) {
+    var randomItems = []
+    for (let index = 0; index < number; index++) {
+        randomItems.push(getRandomItem())        
+    }
+    return randomItems
+}
 
 module.exports = {
     all: starWarsNames,
