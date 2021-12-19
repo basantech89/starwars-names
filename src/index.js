918caa49c21739d25a4f0f47fbd09bb0385c40ca
Basant Soni <basant.soni@velotio.com>
2021-12-19 22:43:34 +0530

    chore(test): testing codecov

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-var uniqueRandomArray = require('unique-random-array')
-var starWarsNames = require('./starwars-names.json')
-var getRandomItem = uniqueRandomArray(starWarsNames)
+var uniqueRandomArray = require("unique-random-array");
+var starWarsNames = require("./starwars-names.json");
+var getRandomItem = uniqueRandomArray(starWarsNames);
 
 function random(number) {
-    var randomItems = []
-    for (let index = 0; index < number; index++) {
-        randomItems.push(getRandomItem())        
-    }
-    return randomItems
+  var randomItems = [];
+  for (let index = 0; index < number; index++) {
+    randomItems.push(getRandomItem());
+  }
+  return randomItems;
 }
 
 module.exports = {
-    all: starWarsNames,
-    random: uniqueRandomArray(starWarsNames)
-}
+  all: starWarsNames,
+  random: uniqueRandomArray(starWarsNames),
+};
 
-// big change
+// big change dd
